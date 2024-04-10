7aef76e2424aa603f1ec8d9cf00a77cadde75821
Reza <rezahandzalah@gmail.com>
2024-04-10 10:32:43 +0700

    parser arc mutex

--- src/main.rs
+++ src/main.rs
@@ -15,56 +15,49 @@ pub struct Z {
     //
     // [the book]: https://doc.rust-lang.org/book/ch16-03-shared-state.html
     ast: Arc<Mutex<Option<Tree>>>,
-    parser: Parser
-    // parser: Arc<Mutex<Parser>>  // ??
+    parser: Arc<Mutex<Parser>>
 }
 
 impl Z {
     fn new() -> Self {
         let mut parser = Parser::new();
         let lang = tree_sitter_rust::language();
-
         parser.set_language(lang).expect("unfit language provided");
 
-        Z { ast: Arc::new(Mutex::new(None)), parser }
+        Z {
+            ast: Arc::new(Mutex::new(None)),
+            parser: Arc::new(Mutex::new(parser))
+        }
     }
 
     // Number of mutation sites: 2
     fn parse(&self, text: impl AsRef<[u8]>) {
-//  fn parse(&mut self, text: impl AsRef<[u8]>) {  // compiles
-        // Rust dance to get concurrent-safe mutable state.
-        let ast = Arc::clone(&self.ast);
-        let mut binding = ast.lock().unwrap();
-        let ast_edit = binding.as_mut();
+        // Rust dance to interact with concurrent-safe mutable state.
+        let parser_ptr = Arc::clone(&self.parser);
+        let mut parser = parser_ptr.lock().unwrap();
 
-        // ??:
-        // let new_ast = match self.parser.parse(text, ast_edit.as_deref()) {  // mutation
-        //     None => panic!("https://docs.rs/tree-sitter/0.20.10/tree_sitter/struct.Parser.html#method.parse"),
-        //     v => v
-        // };
-        // *binding = new_ast;  // mutation
+        let ast_ptr = Arc::clone(&self.ast);
+        let mut ast = ast_ptr.lock().unwrap();
+        let ast_edit: Option<&mut Tree> = ast.as_mut();
+
+        // mutation:
+        *ast = match /* mutation: */ parser.parse(text, ast_edit.as_deref()) {
+            None => panic!("https://docs.rs/tree-sitter/0.20.10/tree_sitter/struct.Parser.html#method.parse"),
+            new_ast => {
+                // println!("\there:\n{:?}", new_ast);
+                new_ast
+            }
+        };
     }
 }
 
 #[tonic::async_trait]
 impl Zer for Z {
-    // fn new() -> Self {
-    // }
-
     async fn replace(
         &self,
-        // &mut self,
         _request: Request<ts_proto::Source>,
     ) -> Result<Response<ts_proto::Reply>, Status> {
-        let _ast = Arc::clone(&self.ast);
-        // let mut edit_tree = ast.lock().expect("todo").as_mut();
-
-        let mut parser = Parser::new();
-        let lang = tree_sitter_rust::language();
-        parser.set_language(lang).expect("unfit language provided");
-        // ?? using it for something, then use request's
-        let _new_source_code = "fn test(a: u32) {}";
-        // self.parse( "fn test(a: u32) {}");
+        self.parse( "fn really_really_long(a: u32) {}");
 
         let reply = ts_proto::Reply { val: 200 };
         Ok(Response::new(reply))
@@ -75,11 +68,8 @@ impl Zer for Z {
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "[::1]:50051".parse()?;
 
-    let mut z = Z::new();
-    z.parse("fn test() {}");
-
     Server::builder()
-        .add_service(ZerServer::new(z))
+        .add_service(ZerServer::new(Z::new()))
         .serve(addr)
         .await?;
 
